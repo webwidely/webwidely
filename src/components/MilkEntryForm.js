75448e1a572cb2ac8e7912e43fd8b3539704a0ab
Abdul Rauf <abdulrauf@Abduls-MacBook-Air.local>
2023-07-09 12:09:04 +0500

    delete function added

--- src/components/MilkEntryForm.js
+++ src/components/MilkEntryForm.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { FaTimes } from 'react-icons/fa';
 
 const MilkEntryForm = () => {
   const [qty, setQty] = useState(2);
-  const [price, setPrice] = useState(160);
+  const [price, setPrice] = useState(320);
   const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
   const [entries, setEntries] = useState([]);
 
@@ -40,8 +41,24 @@ const MilkEntryForm = () => {
       await axios.post('https://pay.webwidely.com/wp-json/jet-cct/milk_entry', data, { headers });
       fetchEntries();
       setQty(2);
-      setPrice(160);
+      setPrice(320);
       setDate(new Date().toISOString().split('T')[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3BheS53ZWJ3aWRlbHkuY29tIiwiaWF0IjoxNjg4ODg0NDM3LCJuYmYiOjE2ODg4ODQ0MzcsImV4cCI6MTY4OTQ4OTIzNywiZGF0YSI6eyJ1c2VyIjp7ImlkIjoiMSJ9fX0.i30wkUBCZ9cz2ZmdhmPbfUyvtDwwCbdiKUVGpKZglIo';
+
+    const headers = {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    };
+
+    try {
+      await axios.delete(`https://pay.webwidely.com/wp-json/jet-cct/milk_entry/${id}`, { headers });
+      fetchEntries();
     } catch (error) {
       console.error(error);
     }
@@ -54,54 +71,97 @@ const MilkEntryForm = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Milk Entry Form</h1>
-      <form onSubmit={handleSubmit} className="mb-4">
-        <div className="flex flex-wrap -mx-2">
-          <div className="w-full md:w-1/3 px-2 mb-4">
-            <label className="block mb-2" htmlFor="qty">
-              Quantity:
-            </label>
-            <input type="text" id="qty" value={qty} onChange={(e) => setQty(e.target.value)} className="border border-gray-300 rounded-md px-2 py-1 w-full" />
-          </div>
-          <div className="w-full md:w-1/3 px-2 mb-4">
-            <label className="block mb-2" htmlFor="price">
-              Price:
-            </label>
-            <input type="text" id="price" value={price} onChange={(e) => setPrice(e.target.value)} className="border border-gray-300 rounded-md px-2 py-1 w-full" />
-          </div>
-          <div className="w-full md:w-1/3 px-2 mb-4">
-            <label className="block mb-2" htmlFor="date">
-              Date:
-            </label>
-            <input type="date" id="date" value={date} onChange={(e) => setDate(e.target.value)} className="border border-gray-300 rounded-md px-2 py-1 w-full" />
-          </div>
+    <div className="container mx-auto p-4 max-w-screen-md">
+      <h1 className="text-3xl font-bold text-center text-green-500 mb-4">Milk Entry Form</h1>
+      <form onSubmit={handleSubmit} className="flex flex-wrap -mx-2 mb-4">
+        <div className="w-full md:w-1/3 px-2 mb-4">
+          <label className="block mb-2" htmlFor="qty">
+            Quantity:
+          </label>
+          <input
+            type="text"
+            id="qty"
+            value={qty}
+            onChange={(e) => setQty(e.target.value)}
+            className="border border-gray-300 rounded-md px-2 py-1 w-full"
+          />
         </div>
-        <button type="submit" className="bg-green-500 hover:bg-green-600 text-white rounded-md px-4 py-2 mt-2">Submit</button>
+        <div className="w-full md:w-1/3 px-2 mb-4">
+          <label className="block mb-2" htmlFor="price">
+            Price:
+          </label>
+          <input
+            type="text"
+            id="price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            className="border border-gray-300 rounded-md px-2 py-1 w-full"
+          />
+        </div>
+        <div className="w-full md:w-1/3 px-2 mb-4">
+          <label className="block mb-2" htmlFor="date">
+            Date:
+          </label>
+          <input
+            type="date"
+            id="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            className="border border-gray-300 rounded-md px-2 py-1 w-full"
+          />
+        </div>
+        <div className="w-full px-2">
+          <button
+            type="submit"
+            className="bg-green-500 hover:bg-green-600 text-white rounded-md px-4 py-2 w-full"
+          >
+            Submit
+          </button>
+        </div>
       </form>
-      <h2 className="text-xl font-bold mb-2">Entries</h2>
+
       <table className="w-full border border-gray-300">
         <thead>
           <tr className="bg-green-500 text-white">
             <th className="px-4 py-2">Quantity</th>
             <th className="px-4 py-2">Price</th>
             <th className="px-4 py-2">Date</th>
+            <th className="px-4 py-2">Actions</th>
           </tr>
         </thead>
         <tbody>
-          {entries.map((entry) => (
-            <tr key={entry.id}>
-              <td className="px-4 py-2">{entry.qty}</td>
-              <td className="px-4 py-2">{entry.price}</td>
-              <td className="px-4 py-2">{entry.date}</td>
-            </tr>
-          ))}
+          {entries.map((entry) => {
+            const formattedDate = new Date(parseInt(entry.date) * 1000).toLocaleDateString('en-US', {
+              month: 'long',
+              day: 'numeric',
+              year: 'numeric',
+            });
+
+            return (
+              <tr key={entry._ID}>
+                <td className="px-4 py-2 text-center">{entry.qty}</td>
+                <td className="px-4 py-2 text-center">{entry.price}</td>
+                <td className="px-4 py-2 text-center">{formattedDate}</td>
+                <td className="px-4 py-2 text-center">
+                  <button
+                    onClick={() => handleDelete(entry._ID)}
+                    className="text-red-500 hover:text-red-700"
+                  >
+                    <FaTimes />
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
         <tfoot>
           <tr className="bg-green-500 text-white">
-            <td className="px-4 py-2">{calculateTotal().totalQty}</td>
-            <td className="px-4 py-2">{calculateTotal().totalPrice}</td>
-            <td className="px-4 py-2">Total</td>
+            <td className="px-4 py-2 text-center">{calculateTotal().totalQty}</td>
+            <td className="px-4 py-2 text-center" colSpan="2">
+              <strong>Total Price: </strong>
+              {calculateTotal().totalPrice}
+            </td>
+            <td className="px-4 py-2"></td>
           </tr>
         </tfoot>
       </table>
